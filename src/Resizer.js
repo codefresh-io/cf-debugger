705bcad788ffc4687773f43383f8f3bced97d43b
andrii.shaforostov <andrii@codefresh.io>
2019-10-02 12:10:16 +0300

    update regex

--- src/Resizer.js
+++ src/Resizer.js
@@ -7,10 +7,10 @@ class Resizer extends Transform {
     }
 
     _transform(data, encoding, callback) {
-        const match = data.toString().match(/^(\\u001b|\u001b)\[8;(\d*);(\d*)t(.*)/i);
+        const match = data.toString().match(/^(\u0007*)(\\u001b|\u001b)\[8;(\d*);(\d*)t(.*)/i);
         if (match) {
-            this.onResize(+match[2], +match[3]);
-            match[4] && this.push(match[4]);
+            this.onResize(+match[3], +match[4]);
+            match[5] && this.push(match[5]);
         } else {
             this.push(data);
         }
