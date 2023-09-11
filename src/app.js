d273e666f049b585ba352ac183c8f00acebc39ac
andrii-codefresh <andrii@codefresh.io>
2023-09-12 00:03:03 +0300

    handle error on exiting, color terminal, raw mode for better handling command keys

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
 const pty = require('node-pty');
 const Resizer = require('./Resizer.js');
 
+process.stdin.setEncoding('utf8');
+process.stdin.setRawMode(true);
+
+let exit = false;
+
 const shell = pty.spawn('/bin/bash', [], {
-    name: 'xterm',
+    name: 'xterm-color',
     cwd: process.env.PWD,
     env: process.env
 });
-shell.on('exit', (code) => {
-    process.exit(code);
+
+process.on('uncaughtException', (err) => {
+    if (exit) {
+        process.exit(0)
+    }
+    console.error(err.message);
+    console.error(err.stack);
 });
 
 const resizerStream = new Resizer(shell.resize.bind(shell));
+
+shell.on('close', () => {
+    exit = true
+})
+
 process.stdin
     .pipe(resizerStream)
     .pipe(shell)
